82912be6e994fb6dabccb5c354a0c605e61006ac
Nguyễn T Thanh Tâm <161405999+thanhtam3704@users.noreply.github.com>
2025-06-04 07:54:38 +0700

    Update AdminMainForm.Designer.cs

--- AdminMainForm.Designer.cs
+++ AdminMainForm.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class AdminMainForm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -19,13 +12,7 @@
             }
             base.Dispose(disposing);
         }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
+        
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminMainForm));
@@ -52,9 +39,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel3.SuspendLayout();
             this.SuspendLayout();
-            // 
             // panel1
-            // 
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.close);
@@ -63,9 +48,7 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1268, 41);
             this.panel1.TabIndex = 0;
-            // 
             // label1
-            // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
@@ -74,9 +57,7 @@
             this.label1.Size = new System.Drawing.Size(227, 20);
             this.label1.TabIndex = 4;
             this.label1.Text = "Hotel Management System";
-            // 
             // close
-            // 
             this.close.AutoSize = true;
             this.close.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.close.ForeColor = System.Drawing.Color.White;
@@ -86,9 +67,7 @@
             this.close.TabIndex = 3;
             this.close.Text = "X";
             this.close.Click += new System.EventHandler(this.close_Click);
-            // 
             // panel2
-            // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(110)))), ((int)(((byte)(138)))));
             this.panel2.Controls.Add(this.bookHotel_btn);
             this.panel2.Controls.Add(this.logout_btn);
@@ -103,9 +82,7 @@
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(258, 714);
             this.panel2.TabIndex = 2;
-            // 
             // bookHotel_btn
-            // 
             this.bookHotel_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(110)))), ((int)(((byte)(138)))));
             this.bookHotel_btn.FlatAppearance.BorderSize = 0;
             this.bookHotel_btn.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ActiveCaption;
@@ -120,9 +97,7 @@
             this.bookHotel_btn.Text = "Book Room";
             this.bookHotel_btn.UseVisualStyleBackColor = false;
             this.bookHotel_btn.Click += new System.EventHandler(this.bookHotel_btn_Click);
-            // 
             // logout_btn
-            // 
             this.logout_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(110)))), ((int)(((byte)(138)))));
             this.logout_btn.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ActiveCaption;
             this.logout_btn.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.ActiveCaption;
@@ -136,9 +111,7 @@
             this.logout_btn.Text = "Logout";
             this.logout_btn.UseVisualStyleBackColor = false;
             this.logout_btn.Click += new System.EventHandler(this.logout_btn_Click);
-            // 
             // customers_btn
-            // 
             this.customers_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(110)))), ((int)(((byte)(138)))));
             this.customers_btn.FlatAppearance.BorderSize = 0;
             this.customers_btn.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ActiveCaption;
@@ -153,9 +126,7 @@
             this.customers_btn.Text = "Customers";
             this.customers_btn.UseVisualStyleBackColor = false;
             this.customers_btn.Click += new System.EventHandler(this.customers_btn_Click);
-            // 
             // addUser_btn
-            // 
             this.addUser_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(110)))), ((int)(((byte)(138)))));
             this.addUser_btn.FlatAppearance.BorderSize = 0;
             this.addUser_btn.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ActiveCaption;
@@ -170,9 +141,7 @@
             this.addUser_btn.Text = "Add User";
             this.addUser_btn.UseVisualStyleBackColor = false;
             this.addUser_btn.Click += new System.EventHandler(this.addUser_btn_Click);
-            // 
             // rooms_btn
-            // 
             this.rooms_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(110)))), ((int)(((byte)(138)))));
             this.rooms_btn.FlatAppearance.BorderSize = 0;
             this.rooms_btn.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ActiveCaption;
@@ -187,9 +156,7 @@
             this.rooms_btn.Text = "Rooms";
             this.rooms_btn.UseVisualStyleBackColor = false;
             this.rooms_btn.Click += new System.EventHandler(this.rooms_btn_Click);
-            // 
             // dashboard_btn
-            // 
             this.dashboard_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(110)))), ((int)(((byte)(138)))));
             this.dashboard_btn.FlatAppearance.BorderSize = 0;
             this.dashboard_btn.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ActiveCaption;
@@ -204,9 +171,7 @@
             this.dashboard_btn.Text = "Dashboard";
             this.dashboard_btn.UseVisualStyleBackColor = false;
             this.dashboard_btn.Click += new System.EventHandler(this.dashboard_btn_Click);
-            // 
             // label5
-            // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.White;
@@ -215,18 +180,14 @@
             this.label5.Size = new System.Drawing.Size(158, 23);
             this.label5.TabIndex = 13;
             this.label5.Text = "Welcome, User";
-            // 
             // pictureBox1
-            // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(85, 28);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(99, 80);
             this.pictureBox1.TabIndex = 12;
-            this.pictureBox1.TabStop = false;
-            // 
+            this.pictureBox1.TabStop = false; 
             // panel3
-            // 
             this.panel3.BackColor = System.Drawing.SystemColors.Control;
             this.panel3.Controls.Add(this.admin_dashboard1);
             this.panel3.Controls.Add(this.admin_addUser1);
@@ -238,44 +199,32 @@
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(1010, 714);
             this.panel3.TabIndex = 3;
-            // 
             // admin_customers1
-            // 
             this.admin_customers1.Location = new System.Drawing.Point(0, 0);
             this.admin_customers1.Name = "admin_customers1";
             this.admin_customers1.Size = new System.Drawing.Size(1010, 714);
             this.admin_customers1.TabIndex = 0;
-            // 
             // staff_bookRoom1
-            // 
             this.staff_bookRoom1.Location = new System.Drawing.Point(0, 0);
             this.staff_bookRoom1.Name = "staff_bookRoom1";
             this.staff_bookRoom1.Size = new System.Drawing.Size(1010, 786);
             this.staff_bookRoom1.TabIndex = 1;
-            // 
             // admin_rooms1
-            // 
             this.admin_rooms1.Location = new System.Drawing.Point(0, 0);
             this.admin_rooms1.Name = "admin_rooms1";
             this.admin_rooms1.Size = new System.Drawing.Size(1010, 714);
             this.admin_rooms1.TabIndex = 2;
-            // 
             // admin_addUser1
-            // 
             this.admin_addUser1.Location = new System.Drawing.Point(0, 0);
             this.admin_addUser1.Name = "admin_addUser1";
             this.admin_addUser1.Size = new System.Drawing.Size(1010, 714);
             this.admin_addUser1.TabIndex = 3;
-            // 
             // admin_dashboard1
-            // 
             this.admin_dashboard1.Location = new System.Drawing.Point(0, 0);
             this.admin_dashboard1.Name = "admin_dashboard1";
             this.admin_dashboard1.Size = new System.Drawing.Size(1010, 714);
             this.admin_dashboard1.TabIndex = 4;
-            // 
             // AdminMainForm
-            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
@@ -294,11 +243,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
-
         }
-
         #endregion
-
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label close;
