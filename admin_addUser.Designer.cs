a544399c0db736b353be884a2b4725f71b063656
Nguyễn T Thanh Tâm <161405999+thanhtam3704@users.noreply.github.com>
2025-06-04 07:58:29 +0700

    Update admin_addUser.Designer.cs

--- admin_addUser.Designer.cs
+++ admin_addUser.Designer.cs
@@ -2,15 +2,7 @@
 {
     partial class admin_addUser
     {
-        /// <summary> 
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
-
-        /// <summary> 
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -19,13 +11,6 @@
             }
             base.Dispose(disposing);
         }
-
-        #region Component Designer generated code
-
-        /// <summary> 
-        /// Required method for Designer support - do not modify 
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
