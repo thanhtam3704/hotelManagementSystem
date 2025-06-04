cbb7506b982fef79de5500c15bf0c1beba3717a8
Nguyễn T Thanh Tâm <161405999+thanhtam3704@users.noreply.github.com>
2025-06-04 07:57:22 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,9 +8,6 @@ namespace hotelManagementSystem
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
